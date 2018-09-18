75ed79a4977488f7e75b93ce643ba856dbeaf880
blackpanter99 <36819304+blackpanter99@users.noreply.github.com>
2018-09-18 15:33:43 +0700

    Update Student.java

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -2,37 +2,75 @@ package week1;
 
 public class Student {
 
-    // TODO: khai báo các thuộc tính cho Student
-
-    // TODO: khai báo các phương thức getter, setter cho Student
-
-    /**
-     * Constructor 1
-     */
+     private String name,id,group,email;
     Student() {
-        // TODO:
+        this.name = "Student";
+        this.id = "000";
+        this.group = "INT22041";
+        this.email ="uet@vnu.edu.com";
     }
-
-    /**
-     * Constructor 2
-     * @param n
-     * @param sid
-     * @param em
-     */
+    
+  
     Student(String n, String sid, String em) {
-        // TODO:
+        name = n;
+        id = sid;
+        email = em;
+        this.group = "INT22041";
+    }
+     Student(String n, String sid, String em,String gr) {
+        name = n;
+        id = sid;
+        email = em;
+        group = gr;
     }
 
     /**
      * Constructor 3
      * @param s
      */
-    Student(Student s) {
-        // TODO:
+   
+   
+     Student(Student s) {
+         this.name = s.name;
+         this.id = s.id;
+         this.email = s.email;
+         this.group = s.group;
+        
+    }
+   
+    public void setName(String name) {
+        this.name = name;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+    
     String getInfo() {
-        // TODO:
-        return null; // xóa dòng này sau khi cài đặt
+        return this.name + " " + this.id + " " + this.group + " " + this.email;
     }
 }
