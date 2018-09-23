fa99c29350dc5f64356f69e0332277d73ed3b4e8
blackpanter99 <36819304+blackpanter99@users.noreply.github.com>
2018-09-23 11:27:35 +0700

    Update Task1.java

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -4,11 +4,33 @@ public class Task1 {
 
     public static int gcd(int a, int b) {
         // TODO: Tính ước chung lớn nhất của 2 số a, b
-        return 0;
+        if(a == b)
+        {
+            return a;
+        }
+        else if(a > b)            
+        {
+            return gcd(a-b,b);
+        }
+        else 
+        {
+            return gcd(a,b-a);
+        }
     }
 
     public static int fibonacci(int n) {
         // TODO: Tìm số fibonacci ở vị trí n
-        return 0;
+         if(n == 0)
+        {
+         return 0;
+        }
+        if(n == 1)
+        {
+          return 1;
+        }
+        else
+        {
+         return fibonacci(n-1) + fibonacci(n-2);
+        }
     }
 }
